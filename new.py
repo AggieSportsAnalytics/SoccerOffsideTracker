5af808e8ec4fc8476c8733196e753f529f836c98
AndrewH06 <andrew.z.hale1@gmail.com>
2023-05-22 12:31:30 -0700

    🧹 Cleaned extra file

--- new.py
+++
@@ -1,8 +0,0 @@
-from roboflow import Roboflow
-
-rf = Roboflow(api_key="V8EhAppumdHyBRAQ9XSY")
-project = rf.workspace().project("football-team-separation")
-model = project.version(15).model
-
-# infer on a local image
-model.predict("img.png", confidence=40, overlap=30).save("prediction.jpg")
